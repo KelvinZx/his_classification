6df1ab991958989060880eceb97a79a99f2b028d
yichezhu <zxmonent@gmail.com>
2018-09-17 01:44:17 +0800

    .

--- loss.py
+++ loss.py
@@ -33,8 +33,9 @@ class WeightCrossEntropy(nn.Module):
         log_probs = self.logsoftmax(inputs)
         targets = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1).data.cpu(), 1)
         targets = targets.cuda()
+        smooth = torch.pow(inputs, 0.5)
         targets = (1 - self.epsilon) * targets
-        loss = (- targets * log_probs * self.weight).mean(0).sum()
+        loss = (- targets * log_probs * self.weight* smooth).mean(0).sum()
         return loss
 
 
